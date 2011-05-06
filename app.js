1fff542cd48aa6470b815529c7c6694e9f53766c
honjo2 <honjo2@gmail.com>
2011-05-06 16:20:28 +0900

    xml,json出力追加

--- app.js
+++ app.js
@@ -7,7 +7,22 @@ var DB_NAME = 'db';
 var COLLECTION_NAME = 'names';
 var LIMIT = 100;
 
+// helpers
+function myfind(collection, cb) {
+    collection.find({}, {sort: [['created', -1]], limit: LIMIT}).toArray(cb);
+}
+
+function generateJson(names) {
+    var ret = { names: [ {_attr: {count: names.length} } ] };
+    for (var i = 0; i < names.length; i++) {
+        ret.names.push({ name: names[i].name });
+    }
+    return ret;
+}
+
 require.paths.unshift('./node_modules');
+
+var XML = require('xml');
 
 var express = require('express');
 var mongodb = require('mongodb');
@@ -37,7 +52,7 @@ app.configure('development', function() {
 app.configure('production', function() {
     app.use(express.errorHandler());
     var env = JSON.parse(process.env.VCAP_SERVICES);
-    var mongo = env['mongodb-1.8'][0]['credentials'];
+    var mongo = env['mongodb-1.8'][0].credentials;
     client = new Db(DB_NAME, new Server(mongo.hostname, mongo.port, {}));
     client.open(function(err, p_client) {
         client.authenticate(mongo.username, mongo.password, function() {
@@ -50,14 +65,20 @@ app.configure('production', function() {
 app.get('/', function(req, res) {
     client.collection(COLLECTION_NAME, function(err, collection) {
         if (req.query.remove) {
-            // collection.removeだとエラーになってしまったのでfindAndModifyを使ってる
-            collection.findAndModify({_id: new BSON.ObjectID(req.query.remove)}, [], {}, {remove: true}, function(err, object) {
-                myfind(collection, function(err, names) {
-                    res.render('index', {
-                        names: names
+            // collection.removeだとエラーになってしまったのでこうしてる
+            collection.findAndModify(
+                { _id: new BSON.ObjectID(req.query.remove) },
+                [],
+                {},
+                { remove: true },
+                function(err, object) {
+                    myfind(collection, function(err, names) {
+                        res.render('index', {
+                            names: names
+                        });
                     });
-                });
-            });
+                }
+            );
         } else {
             myfind(collection, function(err, names) {
                 res.render('index', {
@@ -71,19 +92,38 @@ app.get('/', function(req, res) {
 app.post('/', function(req, res) {
     var name = req.body.name || '';
     client.collection(COLLECTION_NAME, function(err, collection) {
-        collection.insert({name: name, created: new Date().getTime()}, function(err, result) {
-            res.redirect('/');
-        });
-
+        collection.insert(
+            { name: name, created: new Date().getTime() },
+            function(err, result) {
+                res.redirect('/');
+            }
+        );
     });
 });
 
-// helpers
+app.get('/xml', function(req, res) {
+    client.collection(COLLECTION_NAME, function(err, collection) {
+        myfind(collection, function(err, names) {
+            res.send(
+                XML(generateJson(names), true),
+                { 'Content-Type': 'application/xml' },
+                200
+            );
+        });
+    });
+});
 
-function myfind(collection, cb) {
-    collection.find({}, {sort: [['created', -1]], limit: LIMIT}).toArray(cb);
-}
-
+app.get('/json', function(req, res) {
+    client.collection(COLLECTION_NAME, function(err, collection) {
+        myfind(collection, function(err, names) {
+            res.send(
+                JSON.stringify(generateJson(names)),
+                { 'Content-Type': 'application/json' },
+                200
+            );
+        });
+    });
+});
 
 // Only listen on $ node app.js
 
